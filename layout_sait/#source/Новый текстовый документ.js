050190427ff6f1c06732be2347e75150c7251973
kirillserebrykov <60855065+kirillserebrykov@users.noreply.github.com>
2020-03-13 12:20:19 +0200

    Add files via upload

---
+++ layout_sait/#source/Новый текстовый документ.js
@@ -0,0 +1,17 @@
+import annyang;
+
+
+annyang.setLanguage('ru');
+var commands = {
+'привет': function() {
+    document.write('<h3>Приветствую, я голосовой ассистент!</h3>');
+},
+'пока': function() {
+    document.write('<h3>Мы смогли, пока!</h3>');
+}
+};
+
+
+console.log{
+	привет
+}
